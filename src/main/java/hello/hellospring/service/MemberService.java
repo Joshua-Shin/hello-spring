02117baa026484934c71c1aef4b24f962db322d8
sjh910805@gmail.com <sjh910805@gmail.com>
2022-12-11 20:23:18 +0900

    feat: 회원가입, 회원 조회 서비스 구현

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,39 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 중복 이름일 경우 회원가입 X
+        validateDuplicateName(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateName(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("중복된 이름 입니다.");
+                });
+    }
+
+    /**
+     * 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
