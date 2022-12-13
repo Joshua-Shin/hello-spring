a90e596deed45cb849fc19f3a549fd741352e176
sjh910805@gmail.com <sjh910805@gmail.com>
2022-12-13 20:37:43 +0900

    feat: 자바 코드로 직접 스트링 빈 등록

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -8,12 +8,12 @@ import java.util.Map;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public class MemoryMemberRepository implements MemberRepository {
     public static Map<Long, Member> store = new HashMap<>();
     public static long sequence = 0L;
 
-    @Override
+
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
