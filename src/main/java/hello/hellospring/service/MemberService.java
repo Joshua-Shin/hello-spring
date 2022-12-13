22779dd27aa65ac6b235b241307d0711d436fe28
sjh910805@gmail.com <sjh910805@gmail.com>
2022-12-13 19:32:24 +0900

    test: 회원가입 테스트 코드 구현

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -7,7 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package hello.hellospring.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memoryMemberRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        memoryMemberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memoryMemberRepository);
+    }
+    @AfterEach
+    void afterEach() {
+        memoryMemberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+        // when
+        memberService.join(member);
+        Member findMember = memoryMemberRepository.findByName(member.getName()).get();
+        // then
+        assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void 중복_이름_가입() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+        // when
+        memberService.join(member1);
+        // then
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("중복된 이름 입니다.");
+    }
+}
