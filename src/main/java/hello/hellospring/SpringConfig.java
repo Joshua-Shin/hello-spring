f162e2693d4a6eac889a16d0f781ae1c30ff6f43
sjh910805@gmail.com <sjh910805@gmail.com>
2022-12-15 05:11:44 +0900

    feat: 스프링 데이터 JPA로 구현

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -13,23 +13,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    private EntityManager em;
-    private final DataSource dataSource;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em, DataSource dataSource) {
-        this.em = em;
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberReposritory(dataSource);
-        return new JpaMemberRepository(em);
-    }
+
 }
