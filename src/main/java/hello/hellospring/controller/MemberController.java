cf67f4f7da2ba9fe2761794b8c6eb2df622815ca
sjh910805@gmail.com <sjh910805@gmail.com>
2022-12-14 19:04:24 +0900

    feat: 회원 웹 기능 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,8 +3,10 @@ package hello.hellospring.controller;
 import hello.hellospring.MemberForm;
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -26,6 +28,12 @@ public class MemberController {
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 
